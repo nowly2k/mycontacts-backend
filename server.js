102b7ea27cf9f7da0f7b30b94d43cb9bbf507ac0
Marsel Kuzhashev <nowly2k@mail.ru>
2024-05-21 20:38:03 +0300

    connected to DB

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler.js");
-const dotenv = require('dotenv').config()
+const connectDb = require("./config/dbConnection.js");
+const dotenv = require("dotenv").config();
 
-const app = express()
+connectDb();
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use('/api/contacts', require('./routes/contactRoutes.js'))
-app.use(errorHandler)
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes.js"));
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
